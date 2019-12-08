af9ac9d49382fd956219d22682e8cf5cad800f35
Jalal <jalal.albasri@gmail.com>
2019-12-08 10:28:41 +0300

    move tracking below email form

--- admin/client/App/screens/Item/components/EditForm.js
+++ admin/client/App/screens/Item/components/EditForm.js
@@ -476,7 +476,6 @@ var EditForm = React.createClass({
 							{this.renderNameField()}
 							{this.renderKeyOrId()}
 							{this.renderFormElements()}
-							{this.renderTrackingMeta()}
 						</Form>
 					</Grid.Col>
 					<Grid.Col large="one-quarter">
@@ -493,6 +492,16 @@ var EditForm = React.createClass({
 						reference={this.state.values.reference}
 					/>
 				) : null}
+				<Grid.Row>
+					<Grid.Col large="three-quarters">
+						<Form layout="horizontal" component="div">
+							{this.renderTrackingMeta()}
+						</Form>
+					</Grid.Col>
+					<Grid.Col large="one-quarter">
+						<span />
+					</Grid.Col>
+				</Grid.Row>
 				{this.renderFooterBar()}
 				<ConfirmationDialog
 					confirmationLabel="Reset"
